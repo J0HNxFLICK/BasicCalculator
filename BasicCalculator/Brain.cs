0b1923d00671b038fd59f88d31a5fbb740098e61
J0HNxFLICK <nirmal.arun.ashish@gmail.com>
2022-12-06 16:00:05 +0530

    [Arun] Added program with addition capabilities

---
+++ BasicCalculator/Brain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    internal class Brain
+    {
+        public void addition(int first, int second)
+        {
+            int result = first+ second;
+            Console.WriteLine("The addition of the numbers is : "+result);
+        }
+    }
+}
---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BasicCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C A L C U L A T O R ");
+            Brain calculator = new Brain();
+            Console.WriteLine("Enter first value : ");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first value : ");
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            calculator.addition(firstValue, secondValue);
+        }
+    }
+}
